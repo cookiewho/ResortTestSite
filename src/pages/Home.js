fa0672f84fc3f25c8b50f0785b3e0ead7f563410
cookiewho <dinkwho@gmail.com>
2020-08-16 18:49:21 -0700

    set up sections for easy ttraversal in the home page, added services section to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
-
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom"
+import Services from "../components/Services"
 export default function Home() {
-  return <div>hello from home page</div>;
+  return (
+    <React.Fragment>
+    <Hero hero="defaultHero">
+      <Banner title="Luxurious Rooms" subtitle="delux rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          our rooms
+        </Link>{" "}
+      </Banner>
+    </Hero>
+    <Services></Services>
+    </React.Fragment>
+  );
 }

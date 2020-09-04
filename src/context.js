9970712a41dfbc78285ac957f287d390febdbef4
cookiewho <dinkwho@gmail.com>
2020-09-03 18:29:21 -0700

    set up context.js so that we can have values to filter our rooms with in our rooms page

--- src/context.js
+++ src/context.js
@@ -3,18 +3,37 @@ import items from "./data";
 const RoomContext = React.createContext();
 //
 class RoomProvider extends Component {
-  state = { rooms: [], sortedRooms: [], featuredRooms: [], loading: true };
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
+  };
   //getData
 
   componentDidMount() {
     //this.getData
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
 
@@ -33,13 +52,22 @@ class RoomProvider extends Component {
     const room = tempRooms.find((room) => room.slug === slug);
     return room;
   };
-
+  handleChange = (event) => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(type, name, value);
+  };
+  filterRooms = () => {
+    console.log("hello");
+  };
   render() {
     return (
       <RoomContext.Provider
         value={{
           ...this.state,
           getRoom: this.getRoom,
+          handleChange: this.handleChange,
         }}
       >
         {this.props.children}
